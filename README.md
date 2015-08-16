cc0a0575463fe39a76b0368c661aeaa5843ef466
Tomas Trajan <tomas.trajan@gmail.com>
2015-08-17 00:12:00 +0200

    chore(build): init project, add dependencies, add webpack build, add README.md info

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Angular JS ES6 Testing Example
 Enhanced testing of Angular JS 1.X applications using ES6 modules
+
+## Getting started
+
+1. Clone repository `git clone https://github.com/tomastrajan/angular-js-es6-testing-example.git`
+2. Enter project directory `cd angular-js-es6-testing-example`
+3. Install dependencies `npm install`
+
+## Scripts
+
+All scripts are run with `npm run [script]`, for example: `npm run test`.
+
+* `test` - run all tests
+* `start` - start development server, try it by opening `http://localhost:8081/webpack-dev-server/index.html`
+* `build` - create dev build, check `build` directory
+* `dist` - create production build, check `dist` directory
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+var _ = require('lodash');
+var minimist = require('minimist');
+var chalk = require('chalk');
+var webpack = require('webpack');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var paramsDefault = {
+    output: {
+        path: './build',
+        filename: '[name].[hash].js',
+        sourceMapFilename: '[name].[hash].map'
+    },
+    server: {
+        port: 8081
+    }
+};
+var paramsPerTarget = {
+    DEV: {
+        debug: true,
+        devtool: 'inline-source-map',
+        output: {
+            filename: '[name].js'
+        }
+    },
+    BUILD: {
+        debug: true,
+        devtool: 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(['build'])
+        ]
+    },
+    DIST: {
+        debug: false,
+        output: {
+            path: './dist'
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist'])
+        ]
+    }
+};
+var TARGET = minimist(process.argv.slice(2)).TARGET || 'BUILD';
+var params = _.merge(paramsDefault, paramsPerTarget[TARGET]);
+printBuildInfo();
+
+module.exports = {
+    resolve: {
+        extensions: ['', '.js', '.tpl.html']
+    },
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        path: params.output.path,
+        filename: params.output.filename,
+        sourceMapFilename: params.output.sourceMapFilename
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel?optional[]=runtime&stage=1', exclude: /(\.test.js$|node_modules)/},
+            {test: /\.css$/, loader: 'style!css'},
+            {test: /\.tpl.html/, loader: 'html'},
+            {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: "url?limit=50000"}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ].concat(params.plugins || []),
+    devServer: {
+        port: params.server.port
+    },
+    debug: params.debug,
+    devtool: params.devtool,
+    progress: true,
+    colors: true
+};
+
+function printBuildInfo() {
+    console.log('\nStarting ' + chalk.bold.green('"' + TARGET + '"') + ' build');
+    if (TARGET === 'DEV') {
+        console.log('Dev server: ' +
+            chalk.bold.yellow('http://localhost:' + params.server.port + '/webpack-dev-server/index.html') + '\n\n');
+    } else {
+        console.log('\n\n');
+    }
+}
