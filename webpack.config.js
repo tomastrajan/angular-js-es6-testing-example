51ca169ccb1d4279f76205919fa1edd07b99c91a
Tomas Trajan <tomas.trajan@gmail.com>
2015-10-18 16:43:00 +0200

    feat(build): split bundle into app and vendor, paramterize build

--- webpack.config.js
+++ webpack.config.js
@@ -6,67 +6,17 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
-var paramsDefault = {
-    output: {
-        path: './build',
-        filename: '[name].[hash].js',
-        sourceMapFilename: '[name].[hash].map'
-    },
-    server: {
-        port: 8081
-    }
-};
-var paramsPerTarget = {
-    DEV: {
-        debug: true,
-        devtool: 'inline-source-map',
-        output: {
-            filename: '[name].js'
-        }
-    },
-    BUILD: {
-        debug: true,
-        devtool: 'source-map',
-        plugins: [
-            new CleanWebpackPlugin(['build'])
-        ]
-    },
-    DIST: {
-        debug: false,
-        output: {
-            path: './dist'
-        },
-        plugins: [
-            new CleanWebpackPlugin(['dist']),
-            new webpack.optimize.UglifyJsPlugin({
-                mangle: false
-            })
-        ]
-    }
-};
-var TARGET = minimist(process.argv.slice(2)).TARGET || 'BUILD';
-var params = _.merge(paramsDefault, paramsPerTarget[TARGET]);
-printBuildInfo();
-
-module.exports = {
+var PARAMS_DEFAULT = {
     resolve: {
         extensions: ['', '.js', '.tpl.html']
     },
     entry: {
-        main: './src/main.js'
+        main: './src/main.js',
+        vendor: ['lodash', 'jquery', 'bootstrap', 'angular', 'angular-animate', 'angular-ui-bootstrap', 'angular-highlightjs']
     },
     output: {
-        path: params.output.path,
-        filename: params.output.filename,
-        sourceMapFilename: params.output.sourceMapFilename
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/, loader: 'babel?optional[]=runtime&stage=1', exclude: /(\.test.js$|node_modules)/},
-            {test: /\.css$/, loader: 'style!css'},
-            {test: /\.tpl.html/, loader: 'html'},
-            {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url?limit=50000'}
-        ]
+        filename: '[name].[chunkhash].js',
+        sourceMapFilename: '[name].[chunkhash].map'
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -76,23 +26,88 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
-        })
-    ].concat(params.plugins || []),
+        }),
+        new webpack.optimize.DedupePlugin()
+    ],
     devServer: {
-        port: params.server.port
+        port: 8081
     },
-    debug: params.debug,
-    devtool: params.devtool,
     progress: true,
     colors: true
 };
+var PARAMS_PER_TARGET = {
+    DEV: {
+        debug: true,
+        devtool: 'inline-source-map',
+        output: {
+            filename: '[name].js'
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
+        ]
+    },
+    BUILD: {
+        debug: true,
+        output: {
+            path: './build'
+        },
+        devtool: 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(['build']),
+            new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.[chunkhash].js', Infinity)
+        ]
+    },
+    DIST: {
+        debug: false,
+        output: {
+            path: './dist'
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.[chunkhash].js', Infinity),
+            new webpack.optimize.UglifyJsPlugin({
+                mangle: false
+            })
+        ]
+    }
+};
+var TARGET = minimist(process.argv.slice(2)).TARGET || 'BUILD';
+var params = _.merge(PARAMS_DEFAULT, PARAMS_PER_TARGET[TARGET], _mergeArraysCustomizer);
 
-function printBuildInfo() {
+_printBuildInfo(params);
+
+module.exports = {
+    resolve: params.resolve,
+    entry: params.entry,
+    output: params.output,
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel?optional[]=runtime&stage=1', exclude: /(\.test.js$|node_modules)/},
+            {test: /\.css$/, loader: 'style!css'},
+            {test: /\.tpl.html/, loader: 'html'},
+            {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url?limit=50000'}
+        ]
+    },
+    plugins: params.plugins,
+    devServer: params.devServer,
+    debug: params.debug,
+    devtool: params.devtool,
+    progress: params.progress,
+    colors: params.colors
+};
+
+function _printBuildInfo(params) {
     console.log('\nStarting ' + chalk.bold.green('"' + TARGET + '"') + ' build');
     if (TARGET === 'DEV') {
         console.log('Dev server: ' +
-            chalk.bold.yellow('http://localhost:' + params.server.port + '/webpack-dev-server/index.html') + '\n\n');
+            chalk.bold.yellow('http://localhost:' + params.devServer.port + '/webpack-dev-server/index.html') + '\n\n');
     } else {
         console.log('\n\n');
+    }
+}
+
+function _mergeArraysCustomizer(a, b) {
+    if (_.isArray(a)) {
+        return a.concat(b);
     }
 }
